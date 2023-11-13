38c26d9691b0040fd7a9415e186c5f56c3c5d497
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-11-14 00:16:47 +0100

    Move everything over to use Fusion

--- src/main/java/com/supermartijn642/connectedglass/CGPaneBlock.java
+++ src/main/java/com/supermartijn642/connectedglass/CGPaneBlock.java
@@ -4,7 +4,9 @@ import com.supermartijn642.core.block.EditableBlockRenderLayer;
 import net.minecraft.block.PaneBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
 
 /**
  * Created 5/11/2020 by SuperMartijn642
@@ -17,6 +19,10 @@ public class CGPaneBlock extends PaneBlock implements EditableBlockRenderLayer {
     public CGPaneBlock(CGGlassBlock block){
         super(Properties.of(Material.GLASS).sound(SoundType.GLASS).strength(0.3f));
         this.block = block;
+    }
+
+    public static BooleanProperty getConnectionProperty(Direction side){
+        return PROPERTY_BY_DIRECTION.get(side);
     }
 
     @Override
