75847bbdf7fcf4983e18d3384cac06b5cfc332e7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-11-14 00:16:47 +0100

    Move everything over to use Fusion

--- src/main/java/com/supermartijn642/connectedglass/data/CGModelGenerator.java
+++ src/main/java/com/supermartijn642/connectedglass/data/CGModelGenerator.java
@@ -4,7 +4,10 @@ import com.supermartijn642.connectedglass.CGGlassType;
 import com.supermartijn642.core.generator.ModelGenerator;
 import com.supermartijn642.core.generator.ResourceCache;
 import net.minecraft.item.DyeColor;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.function.BiConsumer;
 
 /**
  * Created 26/09/2022 by SuperMartijn642
@@ -19,12 +22,9 @@ public class CGModelGenerator extends ModelGenerator {
     public void generate(){
         // Create the glass block models
         for(CGGlassType type : CGGlassType.values()){
-            this.cubeAll(type.getRegistryName(), new ResourceLocation("connectedglass", type.getRegistryName()));
             this.model("item/" + type.getRegistryName()).parent(type.getRegistryName());
-            for(DyeColor color : DyeColor.values()){
-                this.cubeAll(type.getRegistryName(color), new ResourceLocation("connectedglass", type.getRegistryName(color)));
+            for(DyeColor color : DyeColor.values())
                 this.model("item/" + type.getRegistryName(color)).parent(type.getRegistryName(color));
-            }
         }
         this.cubeAll("tinted_glass", new ResourceLocation("connectedglass", "tinted_glass"));
         this.model("item/tinted_glass").parent("tinted_glass");
@@ -35,26 +35,20 @@ public class CGModelGenerator extends ModelGenerator {
             .particleTexture("#all")
             .element(element ->
                 element.shape(7, 0, 0, 9, 16, 16)
-                    .allFaces(face -> face.texture("#all"))
+                    .allFaces((BiConsumer<Direction,FaceBuilder>)(side, face) -> face.texture(side == Direction.EAST || side == Direction.WEST ? "#all" : "#edge"))
             );
-
 
         // Create the pane models
         for(CGGlassType type : CGGlassType.values()){
             if(type.hasPanes){
-                this.createPaneModels(type.getPaneRegistryName(), type.getRegistryName());
+                this.model("item/" + type.getPaneRegistryName()).parent("pane_item_template")
+                    .texture("all", type.getRegistryName() + "/" + type.getRegistryName())
+                    .texture("edge", type.getRegistryName() + "/" + type.getRegistryName() + "_edge");
                 for(DyeColor color : DyeColor.values())
-                    this.createPaneModels(type.getPaneRegistryName(color), type.getRegistryName(color));
+                    this.model("item/" + type.getPaneRegistryName(color)).parent("pane_item_template")
+                        .texture("all", type.getRegistryName() + "/" + type.getRegistryName(color))
+                        .texture("edge", type.getRegistryName() + "/" + type.getRegistryName(color) + "_edge");
             }
         }
-    }
-
-    private void createPaneModels(String identifier, String texture){
-        this.model(identifier + "_side").parent("minecraft", "block/template_glass_pane_side").texture("pane", texture).texture("edge", texture);
-        this.model(identifier + "_post").parent("minecraft", "block/template_glass_pane_post").texture("pane", texture).texture("edge", texture);
-        this.model(identifier + "_side_alt").parent("minecraft", "block/template_glass_pane_side_alt").texture("pane", texture).texture("edge", texture);
-        this.model(identifier + "_noside").parent("minecraft", "block/template_glass_pane_noside").texture("pane", texture).texture("edge", texture);
-        this.model(identifier + "_noside_alt").parent("minecraft", "block/template_glass_pane_noside_alt").texture("pane", texture).texture("edge", texture);
-        this.model("item/" + identifier).parent("pane_item_template").texture("all", texture);
     }
 }
