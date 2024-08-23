11ff3d4292b8ff2327ca5988b85c732ad1517d9b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-23 21:12:44 +0200

    Add glass to the appropriate `forge:glass/<color>` tags #74

--- src/main/java/com/supermartijn642/connectedglass/data/CGTagGenerator.java
+++ src/main/java/com/supermartijn642/connectedglass/data/CGTagGenerator.java
@@ -1,9 +1,13 @@
 package com.supermartijn642.connectedglass.data;
 
+import com.supermartijn642.connectedglass.CGColoredGlassBlock;
+import com.supermartijn642.connectedglass.CGColoredPaneBlock;
 import com.supermartijn642.connectedglass.CGGlassType;
 import com.supermartijn642.core.generator.ResourceCache;
 import com.supermartijn642.core.generator.TagGenerator;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 
@@ -14,6 +18,23 @@ import java.util.ArrayList;
  */
 public class CGTagGenerator extends TagGenerator {
 
+    @SuppressWarnings("unchecked")
+    private static final TagKey<Block>[] COLORED_GLASS_BLOCK_TAGS = new TagKey[]{
+        Tags.Blocks.GLASS_WHITE, Tags.Blocks.GLASS_ORANGE, Tags.Blocks.GLASS_MAGENTA, Tags.Blocks.GLASS_LIGHT_BLUE, Tags.Blocks.GLASS_YELLOW, Tags.Blocks.GLASS_LIME, Tags.Blocks.GLASS_PINK, Tags.Blocks.GLASS_GRAY, Tags.Blocks.GLASS_LIGHT_GRAY, Tags.Blocks.GLASS_CYAN, Tags.Blocks.GLASS_PURPLE, Tags.Blocks.GLASS_BLUE, Tags.Blocks.GLASS_BROWN, Tags.Blocks.GLASS_GREEN, Tags.Blocks.GLASS_RED, Tags.Blocks.GLASS_BLACK
+    };
+    @SuppressWarnings("unchecked")
+    private static final TagKey<Block>[] COLORED_PANE_BLOCK_TAGS = new TagKey[]{
+        Tags.Blocks.GLASS_PANES_WHITE, Tags.Blocks.GLASS_PANES_ORANGE, Tags.Blocks.GLASS_PANES_MAGENTA, Tags.Blocks.GLASS_PANES_LIGHT_BLUE, Tags.Blocks.GLASS_PANES_YELLOW, Tags.Blocks.GLASS_PANES_LIME, Tags.Blocks.GLASS_PANES_PINK, Tags.Blocks.GLASS_PANES_GRAY, Tags.Blocks.GLASS_PANES_LIGHT_GRAY, Tags.Blocks.GLASS_PANES_CYAN, Tags.Blocks.GLASS_PANES_PURPLE, Tags.Blocks.GLASS_PANES_BLUE, Tags.Blocks.GLASS_PANES_BROWN, Tags.Blocks.GLASS_PANES_GREEN, Tags.Blocks.GLASS_PANES_RED, Tags.Blocks.GLASS_PANES_BLACK
+    };
+    @SuppressWarnings("unchecked")
+    private static final TagKey<Item>[] COLORED_GLASS_ITEM_TAGS = new TagKey[]{
+        Tags.Items.GLASS_WHITE, Tags.Items.GLASS_ORANGE, Tags.Items.GLASS_MAGENTA, Tags.Items.GLASS_LIGHT_BLUE, Tags.Items.GLASS_YELLOW, Tags.Items.GLASS_LIME, Tags.Items.GLASS_PINK, Tags.Items.GLASS_GRAY, Tags.Items.GLASS_LIGHT_GRAY, Tags.Items.GLASS_CYAN, Tags.Items.GLASS_PURPLE, Tags.Items.GLASS_BLUE, Tags.Items.GLASS_BROWN, Tags.Items.GLASS_GREEN, Tags.Items.GLASS_RED, Tags.Items.GLASS_BLACK
+    };
+    @SuppressWarnings("unchecked")
+    private static final TagKey<Item>[] COLORED_PANE_ITEM_TAGS = new TagKey[]{
+        Tags.Items.GLASS_PANES_WHITE, Tags.Items.GLASS_PANES_ORANGE, Tags.Items.GLASS_PANES_MAGENTA, Tags.Items.GLASS_PANES_LIGHT_BLUE, Tags.Items.GLASS_PANES_YELLOW, Tags.Items.GLASS_PANES_LIME, Tags.Items.GLASS_PANES_PINK, Tags.Items.GLASS_PANES_GRAY, Tags.Items.GLASS_PANES_LIGHT_GRAY, Tags.Items.GLASS_PANES_CYAN, Tags.Items.GLASS_PANES_PURPLE, Tags.Items.GLASS_PANES_BLUE, Tags.Items.GLASS_PANES_BROWN, Tags.Items.GLASS_PANES_GREEN, Tags.Items.GLASS_PANES_RED, Tags.Items.GLASS_PANES_BLACK
+    };
+
     public CGTagGenerator(ResourceCache cache){
         super("connectedglass", cache);
     }
@@ -21,11 +42,11 @@ public class CGTagGenerator extends TagGenerator {
     @Override
     public void generate(){
         ArrayList<Block> glass = new ArrayList<>();
-        ArrayList<Block> glassColored = new ArrayList<>();
+        ArrayList<CGColoredGlassBlock> glassColored = new ArrayList<>();
         ArrayList<Block> glassColorless = new ArrayList<>();
         ArrayList<Block> glassTinted = new ArrayList<>();
         ArrayList<Block> panes = new ArrayList<>();
-        ArrayList<Block> panesColored = new ArrayList<>();
+        ArrayList<CGColoredPaneBlock> panesColored = new ArrayList<>();
         ArrayList<Block> panesColorless = new ArrayList<>();
         ArrayList<Block> panesTinted = new ArrayList<>();
 
@@ -52,6 +73,10 @@ public class CGTagGenerator extends TagGenerator {
         glass.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS)::add);
         glassColored.forEach(this.blockTag(Tags.Blocks.STAINED_GLASS)::add);
         glassColored.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.STAINED_GLASS)::add);
+        glassColored.forEach(b -> {
+            this.blockTag(COLORED_GLASS_BLOCK_TAGS[b.getColor().getId()]).add(b);
+            this.itemTag(COLORED_GLASS_ITEM_TAGS[b.getColor().getId()]).add(b.asItem());
+        });
         glassColorless.forEach(this.blockTag(Tags.Blocks.GLASS_COLORLESS)::add);
         glassColorless.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_COLORLESS)::add);
         glassTinted.forEach(this.blockTag(Tags.Blocks.GLASS_TINTED)::add);
@@ -60,6 +85,10 @@ public class CGTagGenerator extends TagGenerator {
         panes.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_PANES)::add);
         panesColored.forEach(this.blockTag(Tags.Blocks.STAINED_GLASS_PANES)::add);
         panesColored.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.STAINED_GLASS_PANES)::add);
+        panesColored.forEach(b -> {
+            this.blockTag(COLORED_PANE_BLOCK_TAGS[b.getColor().getId()]).add(b);
+            this.itemTag(COLORED_PANE_ITEM_TAGS[b.getColor().getId()]).add(b.asItem());
+        });
         panesColorless.forEach(this.blockTag(Tags.Blocks.GLASS_PANES_COLORLESS)::add);
         panesColorless.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_PANES_COLORLESS)::add);
         panesTinted.forEach(this.blockTag("forge", "glass_panes/tinted")::add);
