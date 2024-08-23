dedba79f74312ea994d7bbfecbddc877fc2d5b0a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-08-23 23:36:33 +0200

    Update glass tags

--- src/main/java/com/supermartijn642/connectedglass/data/CGTagGenerator.java
+++ src/main/java/com/supermartijn642/connectedglass/data/CGTagGenerator.java
@@ -7,8 +7,6 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.Tags;
 
-import java.util.ArrayList;
-
 /**
  * Created 5/26/2020 by SuperMartijn642
  */
@@ -20,46 +18,27 @@ public class CGTagGenerator extends TagGenerator {
 
     @Override
     public void generate(){
-        ArrayList<Block> glass = new ArrayList<>();
-        ArrayList<Block> glassColored = new ArrayList<>();
-        ArrayList<Block> glassColorless = new ArrayList<>();
-        ArrayList<Block> glassTinted = new ArrayList<>();
-        ArrayList<Block> panes = new ArrayList<>();
-        ArrayList<Block> panesColored = new ArrayList<>();
-        ArrayList<Block> panesColorless = new ArrayList<>();
-        ArrayList<Block> panesTinted = new ArrayList<>();
-
         for(CGGlassType type : CGGlassType.values()){
-            glass.addAll(type.blocks);
-            if(type.isTinted)
-                glassTinted.addAll(type.blocks);
-            else{
-                glassColored.addAll(type.colored_blocks.values());
-                glassColorless.add(type.block);
+            type.blocks.forEach(this.blockTag(Tags.Blocks.GLASS_BLOCKS)::add);
+            type.blocks.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_BLOCKS)::add);
+            if(type.isTinted){
+                type.blocks.forEach(this.blockTag(Tags.Blocks.GLASS_BLOCKS_TINTED)::add);
+                type.blocks.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_BLOCKS_TINTED)::add);
+            }else{
+                type.blocks.forEach(this.blockTag(Tags.Blocks.GLASS_BLOCKS_CHEAP)::add);
+                type.blocks.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_BLOCKS_CHEAP)::add);
+                this.blockTag(Tags.Blocks.GLASS_BLOCKS_COLORLESS).add(type.block);
+                this.itemTag(Tags.Items.GLASS_BLOCKS_COLORLESS).add(type.block.asItem());
             }
             if(type.hasPanes){
-                panes.addAll(type.panes);
-                if(type.isTinted)
-                    panesTinted.addAll(type.panes);
-                else{
-                    panesColored.addAll(type.colored_panes.values());
-                    panesColorless.add(type.pane);
+                type.panes.forEach(this.blockTag(Tags.Blocks.GLASS_PANES)::add);
+                type.panes.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_PANES)::add);
+                if(!type.isTinted){
+                    this.blockTag(Tags.Blocks.GLASS_PANES_COLORLESS).add(type.pane);
+                    this.itemTag(Tags.Items.GLASS_PANES_COLORLESS).add(type.pane.asItem());
                 }
             }
         }
-
-        glass.forEach(this.blockTag(Tags.Blocks.GLASS_BLOCKS)::add);
-        glass.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_BLOCKS)::add);
-        glassColorless.forEach(this.blockTag(Tags.Blocks.GLASS_BLOCKS_COLORLESS)::add);
-        glassColorless.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_BLOCKS_COLORLESS)::add);
-        glassTinted.forEach(this.blockTag(Tags.Blocks.GLASS_BLOCKS_TINTED)::add);
-        glassTinted.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_BLOCKS_TINTED)::add);
-        panes.forEach(this.blockTag(Tags.Blocks.GLASS_PANES)::add);
-        panes.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_PANES)::add);
-        panesColorless.forEach(this.blockTag(Tags.Blocks.GLASS_PANES_COLORLESS)::add);
-        panesColorless.stream().map(Block::asItem).forEach(this.itemTag(Tags.Items.GLASS_PANES_COLORLESS)::add);
-        panesTinted.forEach(this.blockTag("forge", "glass_panes/tinted")::add);
-        panesTinted.stream().map(Block::asItem).forEach(this.itemTag("forge", "glass_panes/tinted")::add);
 
         // Impermeable tag
         TagBuilder<Block> impermeable = this.blockTag(BlockTags.IMPERMEABLE);
