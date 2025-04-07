19f98585a8a3999e109e7e8264afdbe717913e8b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-04-08 00:04:04 +0200

    Use Fusion for glass pane culling

--- src/main/java/com/supermartijn642/connectedglass/CGPaneBlock.java
+++ src/main/java/com/supermartijn642/connectedglass/CGPaneBlock.java
@@ -3,8 +3,6 @@ package com.supermartijn642.connectedglass;
 import net.minecraft.block.PaneBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.util.Direction;
 
 /**
  * Created 5/11/2020 by SuperMartijn642
@@ -16,9 +14,5 @@ public class CGPaneBlock extends PaneBlock {
     public CGPaneBlock(CGGlassBlock block){
         super(Properties.of(Material.GLASS).sound(SoundType.GLASS).strength(0.3f).noOcclusion());
         this.block = block;
-    }
-
-    public static BooleanProperty getConnectionProperty(Direction side){
-        return PROPERTY_BY_DIRECTION.get(side);
     }
 }
