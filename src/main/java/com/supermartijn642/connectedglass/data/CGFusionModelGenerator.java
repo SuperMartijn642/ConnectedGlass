76f5d230a696a0efdd5071d5b37f20848e592055
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-25 23:17:38 +0200

    Allow pane side to connect up or down only if the pane above/below also has that side

--- src/main/java/com/supermartijn642/connectedglass/data/CGFusionModelGenerator.java
+++ src/main/java/com/supermartijn642/connectedglass/data/CGFusionModelGenerator.java
@@ -1,14 +1,20 @@
 package com.supermartijn642.connectedglass.data;
 
 import com.supermartijn642.connectedglass.CGGlassType;
+import com.supermartijn642.connectedglass.CGPaneBlock;
 import com.supermartijn642.fusion.api.model.DefaultModelTypes;
 import com.supermartijn642.fusion.api.model.ModelInstance;
 import com.supermartijn642.fusion.api.model.data.ConnectingModelData;
+import com.supermartijn642.fusion.api.predicate.ConnectionDirection;
+import com.supermartijn642.fusion.api.predicate.ConnectionPredicate;
 import com.supermartijn642.fusion.api.predicate.DefaultConnectionPredicates;
 import com.supermartijn642.fusion.api.provider.FusionModelProvider;
+import com.supermartijn642.fusion.api.util.Pair;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.item.DyeColor;
 import net.minecraft.util.ResourceLocation;
+
+import java.util.Arrays;
 
 /**
  * Created 26/09/2022 by SuperMartijn642
@@ -48,25 +54,15 @@ public class CGFusionModelGenerator extends FusionModelProvider {
         // Create the pane models
         for(CGGlassType type : CGGlassType.values()){
             if(type.hasPanes){
-                this.createPaneModels(type.getPaneRegistryName(), type.getRegistryName() + "/" + type.getRegistryName());
+                this.createPaneModels(type.getPaneRegistryName(), type.getRegistryName() + "/" + type.getRegistryName(), type.getPane());
                 for(DyeColor color : DyeColor.values())
-                    this.createPaneModels(type.getPaneRegistryName(color), type.getRegistryName() + "/" + type.getRegistryName(color));
+                    this.createPaneModels(type.getPaneRegistryName(color), type.getRegistryName() + "/" + type.getRegistryName(color), type.getPane(color));
             }
         }
     }
 
-    private void createPaneModels(String identifier, String texture){
-        this.addModel(new ResourceLocation("connectedglass", identifier + "_side"),
-            ModelInstance.of(
-                DefaultModelTypes.CONNECTING,
-                ConnectingModelData.builder()
-                    .parent(new ResourceLocation("connectedglass", "block/template_glass_pane_side"))
-                    .texture("pane", new ResourceLocation("connectedglass", texture))
-                    .texture("edge", new ResourceLocation("connectedglass", texture + "_edge"))
-                    .connection(DefaultConnectionPredicates.isSameBlock())
-                    .build()
-            )
-        );
+    @SuppressWarnings("unchecked")
+    private void createPaneModels(String identifier, String texture, CGPaneBlock block){
         this.addModel(new ResourceLocation("connectedglass", identifier + "_post"),
             ModelInstance.of(
                 DefaultModelTypes.CONNECTING,
@@ -78,14 +74,48 @@ public class CGFusionModelGenerator extends FusionModelProvider {
                     .build()
             )
         );
-        this.addModel(new ResourceLocation("connectedglass", identifier + "_side_alt"),
+        ConnectionPredicate notUpOrDown = DefaultConnectionPredicates.isDirection(Arrays.stream(ConnectionDirection.values()).filter(dir -> dir != ConnectionDirection.TOP && dir != ConnectionDirection.BOTTOM).toArray(ConnectionDirection[]::new));
+        this.addModel(new ResourceLocation("connectedglass", identifier + "_side_north"),
+            ModelInstance.of(
+                DefaultModelTypes.CONNECTING,
+                ConnectingModelData.builder()
+                    .parent(new ResourceLocation("connectedglass", "block/template_glass_pane_side"))
+                    .texture("pane", new ResourceLocation("connectedglass", texture))
+                    .texture("edge", new ResourceLocation("connectedglass", texture + "_edge"))
+                    .connection(DefaultConnectionPredicates.matchState(block, Pair.of(CGPaneBlock.NORTH, true)).or(notUpOrDown))
+                    .build()
+            )
+        );
+        this.addModel(new ResourceLocation("connectedglass", identifier + "_side_east"),
+            ModelInstance.of(
+                DefaultModelTypes.CONNECTING,
+                ConnectingModelData.builder()
+                    .parent(new ResourceLocation("connectedglass", "block/template_glass_pane_side"))
+                    .texture("pane", new ResourceLocation("connectedglass", texture))
+                    .texture("edge", new ResourceLocation("connectedglass", texture + "_edge"))
+                    .connection(DefaultConnectionPredicates.matchState(block, Pair.of(CGPaneBlock.EAST, true)).or(notUpOrDown))
+                    .build()
+            )
+        );
+        this.addModel(new ResourceLocation("connectedglass", identifier + "_side_south"),
             ModelInstance.of(
                 DefaultModelTypes.CONNECTING,
                 ConnectingModelData.builder()
                     .parent(new ResourceLocation("connectedglass", "block/template_glass_pane_side_alt"))
                     .texture("pane", new ResourceLocation("connectedglass", texture))
                     .texture("edge", new ResourceLocation("connectedglass", texture + "_edge"))
-                    .connection(DefaultConnectionPredicates.isSameBlock())
+                    .connection(DefaultConnectionPredicates.matchState(block, Pair.of(CGPaneBlock.SOUTH, true)).or(notUpOrDown))
+                    .build()
+            )
+        );
+        this.addModel(new ResourceLocation("connectedglass", identifier + "_side_west"),
+            ModelInstance.of(
+                DefaultModelTypes.CONNECTING,
+                ConnectingModelData.builder()
+                    .parent(new ResourceLocation("connectedglass", "block/template_glass_pane_side_alt"))
+                    .texture("pane", new ResourceLocation("connectedglass", texture))
+                    .texture("edge", new ResourceLocation("connectedglass", texture + "_edge"))
+                    .connection(DefaultConnectionPredicates.matchState(block, Pair.of(CGPaneBlock.WEST, true)).or(notUpOrDown))
                     .build()
             )
         );
